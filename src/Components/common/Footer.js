6c06dd8fdbe72dd371f816279d0e4b886005d555
Andrew Yang <andy.tubeee@gmail.com>
2020-12-18 15:22:03 -0500

    Cleared app.scss, and added height and padding

--- src/Components/common/Footer.js
+++ src/Components/common/Footer.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import '../../Styles/common/Footer.scss'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
 
 const Footer = () => {
     return (
         <div className="Footer">
-            dystic
+            <Row>
+                <Col>
+                    <h1>dystic</h1>
+                    <p>For disabled</p>
+                </Col>
+            </Row>
         </div>
     );
 };
