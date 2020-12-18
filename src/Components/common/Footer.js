cd1f5672b201b05da7ea411eac4cf7c6baa5d2af
Andrew Yang <andy.tubeee@gmail.com>
2020-12-18 15:28:30 -0500

    Started gridbox and positioning

--- src/Components/common/Footer.js
+++ src/Components/common/Footer.js
@@ -2,14 +2,21 @@ import React from 'react';
 import '../../Styles/common/Footer.scss'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
+import 'bootstrap/dist/css/bootstrap.css'
 
 const Footer = () => {
     return (
         <div className="Footer">
             <Row>
-                <Col>
+                <Col className='border' lg={8}>
                     <h1>dystic</h1>
                     <p>For disabled</p>
+                </Col>
+                <Col className="border">
+                    <h3>Contact Us</h3>
+                </Col>
+                <Col className="border">
+                    <h3>Navigate</h3>
                 </Col>
             </Row>
         </div>
