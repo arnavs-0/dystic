855e0d4e91e90ada3cd5b94366b62f4fcfabd728
Andrew Yang <andy.tubeee@gmail.com>
2020-12-21 15:43:41 -0500

    Begin routing with react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,17 +1,28 @@
 import logo from './logo.svg';
 import './App.scss';
+import {Route, Switch, BrowserRouter} from 'react-router-dom'
 
 // Common Components
 import Footer from "./Components/common/Footer";
 import Contact from './Components/Pages/Contact';
 
+// Page Components
+import Register from "./Components/Pages/Register";
+import Login from "./Components/Pages/Login";
+
 function App() {
-  return (
-    <div className="App">
-      <Contact />    
-      <Footer />
-    </div>
-  );
+    return (
+        <div className="App">
+            <BrowserRouter>
+                <Switch>
+                    <Route path="/contact" exact component={Contact}/>
+                    <Route path="/register" exact component={Register}/>
+                    <Route path="/login" exact component={Login}/>
+                </Switch>
+            </BrowserRouter>
+            <Footer/>
+        </div>
+    );
 }
 
 export default App;
