b4bfb77618bf92924c21124843f58ac03cdc5a84
arnavs-0 <sarn248@gmail.com>
2020-12-22 20:52:43 -0500

    Update AboutParagraph.js

--- src/Components/other/About/AboutParagraph.js
+++ src/Components/other/About/AboutParagraph.js
@@ -5,7 +5,13 @@ export default class AboutParagraph extends Component {
         return (
             <div className="container-about-third">
                 <div className="wrap-search">
-                    <p className="paragraph-third"><strong>dystic</strong>, an app for people with disabilities to find jobs that can satisfy and meet your personal condition. With our app, we will specifically show you the available jobs around you as a person with a disability. We are giving you the same financial opportunities as everyone else in the community. Our Al Algorithm can build a resume instantly without you worrying about how to write a good resume. If you can't decide on which job position to apply for, simply save them to your account until you are ready. Having trouble using our application, our human-friendly chatbot that will simulate a real-life job agent, you can get the solutions you need without having too many technical skills. With dystic, finding a stable career with disabilities will no longer be a difficulty in the community. </p>
+                    <p className="paragraph-third"><strong>dystic</strong>, an app for people with disabilities to find jobs that can satisfy and meet your personal condition. 
+                    With our app, we will specifically show you the available jobs around you as a person with a disability. 
+                    We are giving you the same financial opportunities as everyone else in the community. 
+                    Our Al Algorithm can build a resume instantly without you worrying about how to write a good resume. 
+                    If you can't decide on which job position to apply for, simply save them to your account until you are ready. 
+                    Having trouble using our application, our human-friendly chatbot that will simulate a real-life job agent, you can get the solutions you need without having too many technical skills. 
+                    With dystic, finding a stable career with disabilities will no longer be a difficulty in the community. </p>
                 </div>
             </div>
         )
