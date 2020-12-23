111119bc79b14936ef91b92b480c535f9d931cce
Andrew Yang <andy.tubeee@gmail.com>
2020-12-22 23:25:08 -0500

    Regex comment

--- src/Components/Pages/Register.js
+++ src/Components/Pages/Register.js
@@ -18,6 +18,7 @@ function Register(props) {
     }
 
     const validatePassword = (pw) => {
+        // Minimum eight characters, at least one uppercase letter, one lowercase letter, one number and one special character
         const re = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/
         return re.test(pw);
     }
