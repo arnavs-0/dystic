53646db55e64a0b1cddcdc984835eb7135428aee
Andrew Yang <andy.tubeee@gmail.com>
2021-03-01 15:04:29 -0500

    Job-card component progress

--- src/App.js
+++ src/App.js
@@ -10,10 +10,13 @@ import Results from "./Components/Pages/Results";
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import JobCardUnexpand from "./Components/common/JobCardUnexpand";
 
+const jobSummary = "Busy production company is seeking a Motion Graphics/Animator. As part of our creative team you’ll create the amazing images that help visually tell our stories. You'll also be shaping those stories through editing. You’ll be at the forefront of our continued expansion into new types of brand storytelling, including interactive, virtual reality and immersive multimedia experiences."
+const jobType = ["Visual Arts", "Part-Time"]
+
 function App() {
   return (
     <div className="App">
-        <JobCardUnexpand/>
+        <JobCardUnexpand jobTitle="Visual Artist" starCount="4" company={"University of Michigan"} jobLocation={"Dearborn, MI, USA"} jobSummary={jobSummary} jobType={jobType}/>
     </div>
   );
 }
--- src/Components/common/JobCardUnexpand.js
+++ src/Components/common/JobCardUnexpand.js
@@ -1,9 +1,42 @@
 import React from 'react';
+import '@fortawesome/fontawesome-free/css/all.min.css'
+import '../../Styles/common/JobCardUnexpand.scss'
 
 function JobCardUnexpand(props) {
+    let stars = Number(props.starCount);
+    const starComponents = Array(stars).fill('fas fa-star')
     return (
-        <div>
-            <h1>Job Card</h1>
+        <div className={"job-card-container"}>
+            <div className="job-card-row-1">
+                <h1 className="job-card-u-jt mr-5">{props.jobTitle}</h1>
+                <div>
+                    {starComponents.map((cn) => (
+                        <i className={cn}/>
+                    ))
+                    }
+                </div>
+                <button className="job-card-detail-btn">
+                    View Details
+                </button>
+            </div>
+            <div className="job-card-row-2">
+                <p className="job-card-company-title">{props.company}</p>
+                <div className="d-flex">
+                    <i className={'fas fa-map-marker-alt'}/>
+                    <p className="job-card-job-location ml-2">{props.jobLocation}</p>
+                </div>
+            </div>
+            <p className="job-card-summary">
+                Job Summary: {props.jobSummary}
+            </p>
+
+            <div className="job-card-info-cards-container">
+                {
+                    props.jobType.map((type)=>(
+                        <p className={"job-card-job-type"}>{type}</p>
+                    ))
+                }
+            </div>
         </div>
     );
 }
