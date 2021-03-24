aca93d1498bbb9b091b0704a8e9c5daff5c9da0e
Sohil1926 <sohil_a@hotmail.com>
2021-03-23 21:14:35 -0400

    worked on skeleton

--- src/App.js
+++ src/App.js
@@ -9,8 +9,7 @@ import Search from './Components/other/home/Search';
 import Results from "./Components/Pages/Results";
 
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import JobCardUnexpand from "./Components/common/JobCardUnexpand";
-
+import JobCardExpand from "./Components/common/JobCardExpand";
 const jobSummary = " \n Busy production company is seeking a Motion Graphics/Animator. As part of our creative team you’ll create the amazing images that help visually tell our stories. You'll also be shaping those stories through editing. You’ll be at the forefront of our continued expansion into new types of brand storytelling, including interactive, virtual reality and immersive multimedia experiences."
 const jobType = ["Visual Arts", "Part-Time"]
 
@@ -18,7 +17,7 @@ function App() {
   return (
     <div className="App">
       <Results></Results>
-        <JobCardUnexpand jobTitle="Visual Artist" starCount="4" company={"University of Michigan"} jobLocation={"Dearborn, MI, USA"} jobSummary={jobSummary} jobType={jobType}/>
+        <JobCardExpand jobTitle="Visual Artist" starCount="4" company={"University of Michigan"} jobLocation={"Dearborn, MI, USA"} jobSummary={jobSummary} jobType={jobType}/>
     </div>
   );
 }
--- src/Components/common/JobCardExpand.js
+++ src/Components/common/JobCardExpand.js
@@ -1,45 +1,60 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import '@fortawesome/fontawesome-free/css/all.min.css'
-import '../../Styles/common/JobCardUnexpand.scss'
+import '../../Styles/common/JobCardExpand.scss'
 import Skeleton from 'react-loading-skeleton';
 
 
-function JobCardUnexpand(props) {
+function JobCardExpand(props) {
     let stars = Number(props.starCount);
     const starComponents = Array(stars).fill('fas fa-star')
+    const Results = () => {
+    const [error, isLoaded, items, stars] = useState(null);
+
+    useEffect(() => {
+        setTimeout( async () => { 
+            const res = await fetch('127.0.0.1:5000/jobs?q=construction&l=waterloo')
+            const data = await res.json();
+            setitems(data); 
+        }, 5000)
+    })
+}
     return (
-        <div className={"job-card-container"}>
-            <div className="job-card-row-1">
-                <h1 className="job-card-u-jt mr-5">{props.jobTitle}</h1>
-                <div className = "star-rating">
-                    {starComponents.map((cn) => (
-                        <i className={cn}/>
-                    ))
+        <div>
+            {items.map(item => (
+            <div className={"job-card-container"}>
+                <div className="job-card-row-1">                  
+                    <h1 className="job-card-u-jt mr-5">{item.jobTitle}</h1>
+                    <div className = "star-rating">
+                        {starComponents.map((cn) => (
+                            <i className={cn}/>
+                        ))
+                        }
+                    </div>
+                    <button className="job-card-detail-btn">Apply</button>
+                </div>
+                <div className="job-card-row-2">
+                    <p className="job-card-company-title">{item.Company}</p>
+                    <div className="d-flex">
+                        <i className={'fas fa-map-marker-alt'}/>
+                        <p className="job-card-job-location ml-2">{item.Location}</p>
+                    </div>
+                </div>
+                <p className="job-card-summary">
+                    Job Summary: {item.Summary}
+                </p>
+
+                <div className="job-card-job-type-container">
+                    {
+                        item.jobType.map((type)=>(
+                            <p className={"job-card-job-type"}>{type}</p>
+                        ))
                     }
                 </div>
-                <button className="job-card-detail-btn">
-Apply                </button>
             </div>
-            <div className="job-card-row-2">
-                <p className="job-card-company-title">{props.company}</p>
-                <div className="d-flex">
-                    <i className={'fas fa-map-marker-alt'}/>
-                    <p className="job-card-job-location ml-2">{props.jobLocation}</p>
-                </div>
-            </div>
-            <p className="job-card-summary">
-                Job Summary: {props.jobSummary}
-            </p>
-
-            <div className="job-card-job-type-container">
-                {
-                    props.jobType.map((type)=>(
-                        <p className={"job-card-job-type"}>{type}</p>
-                    ))
-                }
-            </div>
+            ))}
         </div>
     );
+
 }
 
-export default JobCardUnexpand;
+export default JobCardExpand;
