993e82e51831c3ca507298aea5b61fe70e90102f
arnavs-0 <sarn248@gmail.com>
2021-03-28 11:25:06 -0400

    Updated Spacing

--- src/App.js
+++ src/App.js
@@ -10,14 +10,23 @@ import Results from "./Components/Pages/Results";
 
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import JobCardExpand from "./Components/common/JobCardExpand";
+import {Col, Container, Row} from "react-bootstrap";
 const jobSummary = " \n Busy production company is seeking a Motion Graphics/Animator. As part of our creative team you’ll create the amazing images that help visually tell our stories. You'll also be shaping those stories through editing. You’ll be at the forefront of our continued expansion into new types of brand storytelling, including interactive, virtual reality and immersive multimedia experiences."
 const jobType = ["Visual Arts", "Part-Time"]
 
 function App() {
   return (
     <div className="App">
-      <Results></Results>
-        <JobCardExpand jobTitle="Visual Artist" starCount="4" company={"University of Michigan"} jobLocation={"Dearborn, MI, USA"} jobSummary={jobSummary} jobType={jobType}/>
+      <Container>
+        <Row>
+            <Col xs={2} md={4} className="col-2 fixed-top one">
+                <Results/>
+            </Col>
+            <Col xs={6} md={7} className="offset-sm-5 two">
+                <JobCardExpand />
+            </Col>
+        </Row>
+      </Container>
     </div>
   );
 }
--- src/Components/common/JobCardExpand.js
+++ src/Components/common/JobCardExpand.js
@@ -1,14 +1,16 @@
-import React, {useEffect, useState} from 'react';
+import React from 'react';
 import '@fortawesome/fontawesome-free/css/all.min.css'
-import '../../Styles/common/JobCardExpand.scss'
-import Skeleton from 'react-loading-skeleton';
-import {Link} from "react-router-dom";
+import '../../Styles/common/JobCardUnexpand.scss'
+import ContentLoader from "react-content-loader";
+import {Button} from "react-bootstrap";
+import { jobData } from "../../backend/data/jobData.js"
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col'
 
-import ContentLoader from "react-content-loader"
+// import {Link} from "react-router-dom";
 
-
-
-export default class JobCardExpand extends React.Component {
+export default class JobCardBack extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
@@ -19,99 +21,111 @@ export default class JobCardExpand extends React.Component {
         };
     }
 
+
     componentDidMount() {
-        fetch("http://127.0.0.1:5000/jobs?jt=construction&jl=michigan&jn=physical")
-            .then(res => res.json())
-            .then(
-                (result) => {
-                    this.setState({
-                        isLoaded: true,
-                        items: result 
-                    });
-                },
-                // Note: it's important to handle errors here
-                // instead of a catch() block so that we don't swallow
-                // exceptions from actual bugs in components.
-               (error) => {
-                    this.setState({
-                        isLoaded: true,
-                        error
-                    });
-                }
-            )
+        // fetch("http://127.0.0.1:5000/jobs?jt=software&jl=atlanta&jn=learning")
+        //     .then(res => res.json())
+        //     .then(
+        //         (result) => {
+        //             this.setState({
+        //                 isLoaded: true,
+        //                 items: result
+        //             });
+        //         },
+        //         // Note: it's important to handle errors here
+        //         // instead of a catch() block so that we don't swallow
+        //         // exceptions from actual bugs in components.
+        //         (error) => {
+        //             this.setState({
+        //                 isLoaded: true,
+        //                 error
+        //             });
+        //         }
+        //     )
+        this.setState({isLoaded: true})
     }
 
-
- 
     render() {
-        const {error, isLoaded, items, stars} = this.state;
+        // function callApi(url) {
+        //     var uri = encodeURI(url)
+        //     fetch('http://127.0.0.1:5000/details?ju=' + uri)
+        //         .then(res => res.json)
+        //         .then((results) => {
+        //             console.log(results)
+        //         })
+        // }
+        const { error, isLoaded, items, stars } = this.state;
         const starComponents = Array(stars).fill('fas fa-star')
         if (error) {
             return <div>Error: {error.message}</div>;
         } else if (!isLoaded) {
             return(
 
-             <ContentLoader 
-    speed={600}
-    height={360}
-    viewBox="0 0 400 160"
-    backgroundColor="#f3f3f3"
-     margin={200}
-    foregroundColor="#ecebeb"
-  
-    
-    >
-    <rect  x="148" y="8" rx="3" ry="3" width="88" height="6" /> 
-    <rect x="148" y="26" rx="3" ry="3" width="52" height="6" /> 
-    <rect x="148" y="56" rx="3" ry="3" width="410" height="6" /> 
-    <rect x="148" y="72" rx="3" ry="3" width="380" height="6" /> 
-    <rect x="148" y="104" rx="3" ry="3" width="300" height="6" /> 
-    <rect x="148" y="120" rx="3" ry="3" width="310" height="6" /> 
-    <rect x="148" y="136" rx="3" ry="3" width="330" height="6" /> 
-    <rect x="148" y="152" rx="3" ry="3" width="350" height="6" /> 
-    <rect x="148" y="168" rx="3" ry="3" width="178" height="6" /> 
-    <rect x="148" y="194" rx="3" ry="3" width="178" height="6" /> 
-    
-   
-  </ContentLoader>)
+                <ContentLoader
+                    speed={600}
+                    height={360}
+                    viewBox="0 0 400 160"
+                    backgroundColor="#f3f3f3"
+                    margin={200}
+                    foregroundColor="#ecebeb"
+
+
+                >
+                    <rect  x="0" y="8" rx="3" ry="3" width="88" height="6" />
+                    <rect x="0" y="26" rx="3" ry="3" width="52" height="6" />
+                    <rect x="0" y="56" rx="3" ry="3" width="230" height="6" />
+                    <rect x="0" y="72" rx="3" ry="3" width="220" height="6" />
+                    <rect x="0" y="104" rx="3" ry="3" width="230" height="6" />
+                    <rect x="0" y="120" rx="3" ry="3" width="230" height="6" />
+                    <rect x="0" y="136" rx="3" ry="3" width="240" height="6" />
+                    <rect x="0" y="152" rx="3" ry="3" width="220" height="6" />
+                    <rect x="0" y="168" rx="3" ry="3" width="150" height="6" />
+                    <rect x="0" y="194" rx="3" ry="3" width="150" height="6" />
+
+
+                </ContentLoader>)
         } else {
             return (
-                <div>
-                    {items.map(item => (
-                        <div className={"job-card-container"}>
-                            <div className="job-card-row-1">
-                                <h1 className="job-card-u-jt mr-5">{item.jobTitle}</h1>
-                                <div className="star-rating">
-                                    {starComponents.map((cn) => (
-                                        <i className={cn}/>
-                                    ))
-                                    }
-                                </div>
-                                <button className="job-card-detail-btn">Apply</button>
-                            </div>
-                            <div className="job-card-row-2">
-                                <p className="job-card-company-title">{item.Company}</p>
-                                <div className="d-flex">
-                                    <i className={'fas fa-map-marker-alt'}/>
-                                    <p className="job-card-job-location ml-2">{item.Location}</p>
-                                </div>
-                            </div>
-                            <p className="job-card-summary">
-                                Job Summary: {item.Summary}
-
-                                
-                            </p>
-
-                            <div className="job-card-job-type-container">
-                                {
-                                    item.jobType.map((type) => (
-                                        <p className={"job-card-job-type"}>{type}</p>
-                                    ))
-                                }
-                            </div>
+              <div>
+                {jobData.map((item) => (
+                  <div className={'job-card-container m-5'}>
+                    <div className={'m-1'}>
+                      <div className='job-card-row-1'>
+                        <h1 className='job-card-u-jt mr-5'>{item.JobTitle}</h1>
+                        <div class='ratingstars'>
+                          {starComponents.map((cn) => (
+                            <i className={cn} />
+                          ))}
                         </div>
-                    ))}
-                </div>
+                        <Button
+                          onClick={console.log('Hello')}
+                          role='button'
+                          className='job-card-detail-btn'
+                          href='/'
+                        >
+                          View Details
+                        </Button>
+                      </div>
+                      <div className='job-card-row-2'>
+                        <p className='job-card-company-title'>{item.Company}</p>
+                        <div className='d-flex'>
+                          <i className={'fas fa-map-marker-alt'} />
+                          <p className='job-card-job-location ml-2'>
+                            {item.Location}
+                          </p>
+                        </div>
+                      </div>
+                      <p className='job-card-summary'>
+                        Job Summary: {item.Summary}
+                      </p>
+
+                      <div className='job-card-job-type-container'>
+                        {<p className={'job-card-job-type'}>{item.Remote}</p>}
+                      </div>
+                    </div>
+                  </div>
+                ))}
+              </div>
             );
         }
     }
