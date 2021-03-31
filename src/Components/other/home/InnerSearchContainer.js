c4b862386f6e1793b27bff1f22cadc882b1559f2
arnavs-0 <sarn248@gmail.com>
2021-03-31 11:53:55 -0400

    Updated Frontend of search and navbar ui

--- src/Components/other/home/InnerSearchContainer.js
+++ src/Components/other/home/InnerSearchContainer.js
@@ -13,42 +13,6 @@ export default class Check extends Component {
                 </div>
                 <Row className="mt-2">
                     <Col>
-                        {/*<Checkboxes wrapper="wrapper" formClass="state" */}
-                        {/*    name="app" id="a" value="a" */}
-                        {/*    labelClass="label" indicator="indicator" */}
-                        {/*    spanClass="text-radio"*/}
-                        {/*    span="Vision Impairment" */}
-                        {/*/>*/}
-                        {/*<Checkboxes wrapper="wrapper" formClass="state" */}
-                        {/*    name="app" id="b" value="b" */}
-                        {/*    labelClass="label" indicator="indicator" */}
-                        {/*    spanClass="text-radio"*/}
-                        {/*    span="Neurological Impairment" */}
-                        {/*/>*/}
-                        {/*<Checkboxes wrapper="wrapper" formClass="state" */}
-                        {/*    name="app" id="c" value="c" */}
-                        {/*    labelClass="label" indicator="indicator" */}
-                        {/*    spanClass="text-radio"*/}
-                        {/*    span="Psychiatric Impairment" */}
-                        {/*/>*/}
-                        {/*<Checkboxes wrapper="wrapper" formClass="state" */}
-                        {/*    name="app" id="d" value="d" */}
-                        {/*    labelClass="label" indicator="indicator" */}
-                        {/*    spanClass="text-radio"*/}
-                        {/*    span="Learning Impairment" */}
-                        {/*/>*/}
-                        {/*<Checkboxes wrapper="wrapper" formClass="state" */}
-                        {/*    name="app" id="e" value="e" */}
-                        {/*    labelClass="label" indicator="indicator" */}
-                        {/*    spanClass="text-radio"*/}
-                        {/*    span="Hearing Impairment" */}
-                        {/*/>*/}
-                        {/*<Checkboxes wrapper="wrapper" formClass="state" */}
-                        {/*    name="app" id="f" value="f" */}
-                        {/*    labelClass="label" indicator="indicator" */}
-                        {/*    spanClass="text-radio"*/}
-                        {/*    span="Other" */}
-                        {/*/>*/}
                         <Row>
                         <Col md={'auto'}>
                         <ul>
--- src/Components/other/home/Search.js
+++ src/Components/other/home/Search.js
@@ -1,33 +1,110 @@
-import React, { Component } from 'react'
-import { Col, Form, Row } from 'react-bootstrap'
+import React, { createRef, useState } from 'react'
+import { Col, Form, Row, Container, Button, Image } from 'react-bootstrap'
 import '../../../Styles/Pages/Home/Search.scss'
-import InnerSearchContainer from './InnerSearchContainer'
 import InputText from '../../common/InputText'
-import SearchButton from './SearchButton'
+import '../../../Styles/other/checks.scss'
+import img from '../../../assets/img/search_img.svg'
 
-export class Search extends Component {
-    render() {
+export function Search() {
+        const [isVisionChecked, setVisionChecked] = useState(false);
+        const [isPhysicalChecked, setPhysicalChecked] = useState(false);
+        const [isLearningChecked, setLearningChecked] = useState(false);
+        const [isOtherChecked, setOtherChecked] = useState(false);
+        function handleSearch(){
+            console.log(jobInput.current.value);
+            console.log(locationInput.current.value);
+            console.log(isVisionChecked)
+            console.log(isPhysicalChecked)
+            console.log(isLearningChecked)
+            console.log(isOtherChecked)
+        }
+        let jobInput = createRef();
+        let locationInput = createRef();
         return (
             <div className="container-search">
-                <div className="wrap-search">
-                    <Form className="search-form">
+                <Container fluid>
+                    <Row>
+                        <Col>
                         <Row>
-                            <Col>
-                                <InputText  wrapper="wrap-input-search" inputClass="input shadow-none" name="job" input="Job Title" focus="focus-input" />
+                            <span className="search-form-title text-left">Find your <br/> next opportunity <br/> today.</span>
+                        </Row>
+                        <Row className="mb-3">
+                            <Col md="auto">
+                                <span className="search-form-text text-left">Find Jobs According to your needs simply <br/> click on search and choose jobs based on <br/> your interests</span>
                             </Col>
-                            <Col>
-                                <InputText  wrapper="wrap-input-search" inputClass="input shadow-none" name="location" input="Location" focus="focus-input" />
-                            </Col>
-                            <Col>
-                                <SearchButton />
+                            <Col md="auto">
+                                <div className="wrap-build text-center">
+                                    <span className="search-form-text text-center">or build your resume</span>
+                                    <Button className="build-btn" onClick={() => window.location.replace('https://dystic-test.web.app/')}>build</Button>
+                                </div>
                             </Col>
                         </Row>
-                        <div style={{position:"relative", bottom:"180px"}}><InnerSearchContainer /></div>
-                    </Form>
-                </div>
+                        <Row>
+                        <div className="wrap-search">
+                            <Form className="search-form">
+                                <Row>
+                                    <Col>
+                                        <InputText  reference={jobInput}  wrapper="wrap-input-search" inputClass="input shadow-none" name="job" input="Job Title" focus="focus-input" />
+                                    </Col>
+                                    <Col>
+                                        <InputText reference={locationInput} wrapper="wrap-input-search" inputClass="input shadow-none" name="location" input="Location" focus="focus-input" />
+                                    </Col>
+                                    <Col>
+                                        <div className="container-search-form-btn">
+                                            <Button className="search-form-btn shadow-none" onClick={handleSearch}>
+                                                Search
+                                            </Button>
+                                        </div>
+                                    </Col>
+                                </Row>
+                                <div style={{position:"relative", bottom:"180px"}}>
+                                    <div className="ml-2">
+                                        <div>
+                                            <span className="text"><strong>What type of impairment do you have?</strong></span>
+                                        </div>
+                                        <Row className="mt-2">
+                                            <Col>
+                                                <Row>
+                                                    <Col md={'auto'}>
+                                                        <ul>
+                                                            <li>
+                                                                <input id="c1" type="checkbox" onChange={() => isVisionChecked ? setVisionChecked(false) : setVisionChecked(true)}/>
+                                                                <label htmlFor="c1">&nbsp; Vision</label>
+                                                            </li>
+                                                            <li>
+                                                                <input id="c2" type="checkbox" onChange={() => isLearningChecked ? setLearningChecked(false) : setLearningChecked(true)}/>
+                                                                <label htmlFor="c2">&nbsp; Learning</label>
+                                                            </li>
+                                                        </ul>
+                                                    </Col>
+                                                    <Col md={'auto'}>
+                                                        <ul>
+                                                            <li>
+                                                                <input id="c3" type="checkbox" onChange={() => isPhysicalChecked ? setPhysicalChecked(false) : setPhysicalChecked(true)}/>
+                                                                <label htmlFor="c3">&nbsp; Physical</label>
+                                                            </li>
+                                                            <li>
+                                                                <input id="c4" type="checkbox" onChange={() => isOtherChecked ? setOtherChecked(false) : setOtherChecked(true)}/>
+                                                                <label htmlFor="c4">&nbsp; Other</label>
+                                                            </li>
+                                                        </ul>
+                                                    </Col>
+                                                </Row>
+                                            </Col>
+                                        </Row>
+                                    </div>
+                                </div>
+                            </Form>
+                        </div>
+                        </Row>
+                        </Col>
+                        <Col md={'auto'} className="mt-5">
+                            <Image src={img} fluid />
+                        </Col>
+                    </Row>
+                </Container>
             </div>
         )
-    }
 }
 
 export default Search
